27b7b03d28d9320b2b50c7cf3189bf946ae038ac
Vinicius Fortunato <vinicius.roberto@endurance.com>
2022-03-10 21:04:36 -0300

    Setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <p>Hello!</p>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer>
+            <p>All rights reserved.</p>
+        </footer>
+    )
+}
+
+export default Footer; 

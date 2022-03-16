fda756f84e5037cac16617abefe508abe4baa73c
Vinicius Fortunato <vinicius.roberto@endurance.com>
2022-03-16 08:51:26 -0300

    Added customize-cra

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,6 @@
-const rewireStyledComponents = require('react-app-rewire-styled-components');
+const { useBabelRc, override } = require("customize-cra");
 
-module.exports = function override(config, env) {
-  let displayName = false;
-  if (env === 'development') {
-    displayName = true;
-  }
-
-  config = rewireStyledComponents(config, env, {
-    displayName,
-  });
-
-  return config;
-}
+module.exports = override(
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useBabelRc()
+);
